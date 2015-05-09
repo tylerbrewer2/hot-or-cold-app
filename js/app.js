77790ec9f5b0dbd7e8b18d97d56d76c910479f53
georgiaboy02 <tbrewer1@mail.mvnu.edu>
2015-05-09 18:10:32 -0400

    fixed reload issue

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function(){
-	var feedback = "h2#feedback";
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -14,30 +13,36 @@ $(document).ready(function(){
 
   	/*---Game Code---*/
   	var compNum = Math.floor(Math.random()* 101);
-  	var guess = function (input) {
-  		var distance = input - compNum;
-  		if (distance < -1) {
+	$("input#guessButton.button").mousedown(function() {
+ 		var userInput = $("input#userGuess.text").val();
+  		var feedback = "h2#feedback";
+ 	 	var guess = function (input) {
+  			var distance = input - compNum;
+  			if (distance < -1) {
   			distance *= -1
-  		}
-  		if (distance > 50) {
+  			}
+  			if (distance > 50) {
   			$(feedback).replaceWith("<h2 id='feedback'>Ice Cold</h2>");
-  		}
-  		else if (distance > 30) {
-  			$(feedback).replaceWith("<h2 id='feedback'>Cold</h2>");
-  		}
-  		else if (distance > 20) {
-  			$(feedback).replaceWith("<h2 id='feedback'>Warm</h2>");
-  		}
-  		else if (distance > 10) {
-  			$(feedback).replaceWith("<h2 id='feedback'>Hot/h2>");
-  		}
-  		else if (distance >= 1) {
-  			$(feedback).replaceWith("<h2 id='feedback'>Very Hot</h2>");
-  		}
-  		else {
-  			$(feedback).replaceWith("<h2 id='feedback'>You got it!</h2>");
-  		}
-  	}
+  			}
+  			else if (distance > 30) {
+  				$(feedback).replaceWith("<h2 id='feedback'>Cold</h2>");
+  			}
+  			else if (distance > 20) {
+  				$(feedback).replaceWith("<h2 id='feedback'>Warm</h2>");
+  			}
+  			else if (distance > 10) {
+  				$(feedback).replaceWith("<h2 id='feedback'>Hot</h2>");
+  			}
+  			else if (distance >= 1) {
+  				$(feedback).replaceWith("<h2 id='feedback'>Very Hot</h2>");
+  			}
+  			else {
+  				$(feedback).replaceWith("<h2 id='feedback'>You got it!</h2>");
+  			}
+  		};
+	return guess(userInput);
+});
+
 
 });
 
