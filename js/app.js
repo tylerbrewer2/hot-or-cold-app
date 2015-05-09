2882d5fd9d62e32fe34d94c33c550d8a5ab0b3ef
georgiaboy02 <tbrewer1@mail.mvnu.edu>
2015-05-09 17:14:25 -0400

    set up responses in browser

--- js/app.js
+++ js/app.js
@@ -13,8 +13,31 @@ $(document).ready(function(){
 
   	/*---Game Code---*/
   	var compNum = Math.floor(Math.random()* 101);
-
-
+  	var guess = function (input) {
+  		var distance = input - compNum;
+  		if (distance < -1) {
+  			distance *= -1
+  		}
+  		if (distance > 50) {
+  			console.log("Ice cold");
+  		}
+  		else if (distance > 30) {
+  			console.log("Cold");
+  		}
+  		else if (distance > 20) {
+  			console.log("Warm");
+  		}
+  		else if (distance > 10) {
+  			console.log("Hot");
+  		}
+  		else if (distance >= 1) {
+  			console.log("very hot");
+  		}
+  		else {
+  			console.log("You got it!")
+  		}
+  	}
+  	console.log(guess(1));
 
 });
 
